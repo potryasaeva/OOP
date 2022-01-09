83f2cc740ff48dd1a181d81f38a7d9644647bc48
Lidiia <l.potryasaeva@gmail.com>
2022-01-09 15:47:55 +0300

    added project with building class

---
+++ OOP/Building/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Building
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            BuildingClass building = new BuildingClass(963, 3, 60, 5);
+            Console.WriteLine(building.UniqueNumber);
+            Console.WriteLine($"Floors height is {building.CalculateFloorHigh()}");
+            Console.WriteLine($"Quantity of apartments in entrance is {building.CalculateQuantityOfApartmentsInEntrance()}");
+            Console.WriteLine($"Quantity of apartments on the floor is {building.CalculateQuantityOfApartmentsOnFloor()}");
+
+            //проверка уникальности номера
+            BuildingClass building1 = new BuildingClass(963, 3, 60, 5);
+            Console.WriteLine(building1.UniqueNumber);
+
+            BuildingClass building2 = new BuildingClass(963, 3, 60, 5);
+            Console.WriteLine(building2.UniqueNumber);
+
+            BuildingClass building3 = new BuildingClass(963, 3, 60, 5);
+            Console.WriteLine(building3.UniqueNumber);
+
+        }
+    }
+}
