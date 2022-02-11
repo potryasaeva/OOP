ddb48203bca0277a43de31746ad2ea4927e67116
Lidiia <l.potryasaeva@gmail.com>
2022-02-11 15:23:08 +0300

    7 lesson

---
+++ OOP/CoderProject/ACoder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoderProject
+{
+    public class ACoder : IСoder
+    {
+        private readonly char[] _charArray;
+
+        public ACoder(string inputString)
+        {
+            _charArray = inputString.ToCharArray();
+        }
+
+        public string Decode()
+        {
+            for (int i = 0; i < _charArray.Length; i++)
+            {
+                _charArray[i] = (char)(_charArray[i] - 1);
+            }
+
+            return string.Concat(_charArray);
+        }
+
+        public string Encode()
+        {
+            for (int i = 0; i < _charArray.Length; i++)
+            {
+                _charArray[i] = (char)(_charArray[i] + 1);
+            }
+
+            return string.Concat(_charArray);
+        }
+    }
+}
---
+++ OOP/CoderProject/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CoderProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Enter string line for encode/decode");
+            var inputString = Console.ReadLine();
+
+            var ACoder = new ACoder(inputString);
+
+            Console.WriteLine($"ACoder.Encode = {ACoder.Encode()}");
+            Console.WriteLine($"ACoder.Decode = {ACoder.Decode()}");
+
+
+            var BCoder = new BCoder(inputString);
+
+            Console.WriteLine($"BCoder.Encode = {BCoder.Encode()}");
+            Console.WriteLine($"BCoder.Decode = {BCoder.Decode()}");
+
+            Console.ReadKey();
+        }
+    }
+}
